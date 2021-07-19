bb115f5d81a100b73851b80136302eed821ba417
dschratl <hi@dominik-schratl.at>
2021-07-19 13:25:09 +0200

    001: Multiples of 3 and 5

---
+++ 01_multiplesOf3And5.js
@@ -0,0 +1,15 @@
+const multiplesOf3and5 = (number) => {
+    let sum = 0
+    for(let i = 3; i < number; i++) {
+        if(i % 3 == 0 || i % 5 == 0){
+            sum += i
+        } 
+    }
+    return sum
+}
+
+console.log(multiplesOf3and5(10))
+console.log(multiplesOf3and5(49))
+console.log(multiplesOf3and5(1000))
+console.log(multiplesOf3and5(8456))
+console.log(multiplesOf3and5(19564))
