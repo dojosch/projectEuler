4f10383ad83b28d1b3ca67b9f8129c820655be2a
dschratl <hi@dominik-schratl.at>
2021-08-02 12:15:04 +0200

    15

---
+++ 15_latticePaths.js
@@ -0,0 +1,16 @@
+const fact = (num) => {
+    let rval = 1;
+    for (let i = 2; i <= num; i++) {
+        rval*= i;
+    }     
+    return rval;
+}
+
+const latticePaths = (n) => {
+    return fact((2*n))/(fact(n) * fact(n))
+}
+
+
+console.log(latticePaths(4))
+console.log(latticePaths(9))
+console.log(latticePaths(20))
